1648dfe3b7c30b946010313680c5fee786d7cac2
Antti Koikkalainen <antti.koikkalainen59@gmail.com>
2024-02-21 15:45:09 +0200

    edited text

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ player = 'X'
 game_over = False
 
 def check_winner():
-    # Define winning combinations
+    #Defining winning combinations
     winning_combinations = [
-        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Rows
-        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Columns
-        (0, 4, 8), (2, 4, 6)               # Diagonals
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  #Rows
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  #Columns
+        (0, 4, 8), (2, 4, 6)              #Diagonals
     ]
 
     for combo in winning_combinations:
         if board[combo[0]] == board[combo[1]] == board[combo[2]] != '':
-            return board[combo[0]]  # Return the winning player
+            return board[combo[0]]  #Return the winning player here
 
-    return None  # No winner yet
+    return None  #No winner yet
 
 @app.route('/')
 def index():
